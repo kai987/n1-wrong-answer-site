f40f3fc6ab1b25014ba27d6aa386b8e7d3901fcc
Kai <65030330+kai987@users.noreply.github.com>
2026-09-01 05:16:27 +0900

    refactor: centralize seed and auth policy constants

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,13 @@
 export const INTERVALS = [1, 3, 7, 14, 30];
 export const DEFAULT_SOURCE_EXAM = '2025-12';
+export const SEED_VERSION = 1;
 export const CATEGORIES = new Set(['文字・語彙', '文法', '読解']);
 export const SOURCE_RE = /^\d{4}-(0[1-9]|1[0-2])$/;
+
+export const AUTH_POLICY = Object.freeze({
+  registrationEnabled: true,
+  registrationMinPasswordLength: 12,
+});
 
 export const IMPORT_LIMITS = Object.freeze({
   fileBytes: 2 * 1024 * 1024,
