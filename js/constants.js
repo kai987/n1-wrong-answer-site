da752e3b8067a240c5a7d81cca97e78b13b40d98
Kai <65030330+kai987@users.noreply.github.com>
2026-09-08 17:31:09 +0900

    data: bump seed version for reading passages

--- js/constants.js
+++ js/constants.js
@@ -1,6 +1,6 @@
 export const INTERVALS = [1, 3, 7, 14, 30];
 export const DEFAULT_SOURCE_EXAM = '2025-12';
-export const SEED_VERSION = 1;
+export const SEED_VERSION = 2;
 export const CATEGORIES = new Set(['文字・語彙', '文法', '読解']);
 export const SOURCE_RE = /^\d{4}-(0[1-9]|1[0-2])$/;
 
